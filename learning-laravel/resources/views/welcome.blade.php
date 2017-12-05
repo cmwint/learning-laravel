48064b1256c8ad1d824eca1dcb26e5df3c4f60fb
Corinne Winthrop <corinne@bythepixel.com>
2017-12-05 12:40:28 -0700

    Add new route

--- learning-laravel/resources/views/welcome.blade.php
+++ learning-laravel/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel hi hello
                 </div>
 
                 <div class="links">
