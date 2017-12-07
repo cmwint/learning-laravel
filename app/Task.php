f4fdfd0b2f2da090a8fc8fc0fa1484526046c546
Corinne Winthrop <corinne@bythepixel.com>
2017-12-07 11:47:12 -0700

    Add new query scope for incomplete tasks

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    public function scopeIncomplete($query) // task::incomplete, but also treat as query scope
+    {
+        // taking a basic query and wrapping it in a method,
+        // that describes exactly what it does
+        return $query->where('completed', 0);
+
+        // a query scope is just a wrapper around a particular query
+        // allows you to write things like this in PHP artisan tinker
+            //App\Task::incomplete()->where('id', '>=', 2)->get();
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,28 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+use App\Task; // namespace, directoy structure, import task class
+
+Route::get('/', function () {
+    $tasks = DB::table('tasks')->latest()->get();
+
+    // return $tasks;
+    return view('welcome', compact('tasks'));
+});
+
+Route::get('/tasks', function () {
+    // $tasks = DB::table('tasks')->find($id); // query builder
+    $tasks = Task::all(); // using eloquent in a dedicated class
+
+    // return $tasks;
+    return view('tasks.index', compact('tasks'));
+});
+
+Route::get('/tasks/{task}', function ($id) {
+    $task = Task::find($id);
+    // Task::incomplete();
+    // dd($tasks);
+    
+    return view('tasks.show', compact('task'));
+});
