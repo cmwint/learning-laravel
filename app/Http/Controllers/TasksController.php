cd65f47ce03a7f7db78b40c79cf4ea1b1da5df95
Corinne Winthrop <corinne@bythepixel.com>
2017-12-07 14:10:27 -0700

    Use controllers

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,16 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use App\Task;
+use App\Task;  // namespace, directoy structure, import task class
 
 class TasksController extends Controller
 {
-    public function index(
+    public function index()
     {
         // $tasks = DB::table('tasks')->find($id); // query builder
         $tasks = Task::all(); // using eloquent in a dedicated class
 
         // return $tasks;
         return view('tasks.index', compact('tasks'));
+    }
+
+    public function show($id) //accept id
+    {
+        $task = Task::find($id);
+        // Task::incomplete();
+        // dd($tasks);
+        
+        return view('tasks.show', compact('task'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,14 @@
 |
 */
 
-Route::get('tasks', 'TasksController@index');
-
-use App\Task; // namespace, directoy structure, import task class
 
 Route::get('/', function () {
     $tasks = DB::table('tasks')->latest()->get();
-
+    
     // return $tasks;
     return view('welcome', compact('tasks'));
 });
 
-Route::get('/tasks/{task}', function ($id) {
-    $task = Task::find($id);
-    // Task::incomplete();
-    // dd($tasks);
-    
-    return view('tasks.show', compact('task'));
-});
+Route::get('/tasks', 'TasksController@index');
+
+Route::get('/tasks/{task}', 'TasksController@show');
